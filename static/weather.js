349443996d51d778f0ad22475fd6a1d51d88003c
Judah <judahtrembe@gmail.com>
2020-12-08 20:32:57 +0100

    touch start was the answer

--- static/weather.js
+++ static/weather.js
@@ -6,66 +6,85 @@ const list = document.querySelector('ul');
 // define empty arr for holding, for checking if a city is already on the page
 var dataArr = []
 
-// submit gets click event, unable to listen for submit events from mobile users
-submit.addEventListener('click', el => {
-    // prevent form from submitting
-    el.preventDefault();
+// factor a main funciton to be called for multiple events
+function main(){
     const usrInput = document.querySelector('#userInput').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${usrInput}&appid=${KEY}&units=metric`;
-    fetch(url)
-      .then(res => res.json())
-        .then(data=>{
-            // data from api 
-            const {main, name, sys, weather} = data;
-            const icon = `https://openweathermap.org/img/wn/${weather[0]['icon']}@2x.png`;
-            // markup for weather card
-            const markup = `
-            <li class="city card col-12">
-                <h2 class="city-name card-title">
-                    ${name}, ${sys.country}
-                </h2>
-                <h3 class="city-temp display-4">${Math.round(main.temp)}<sup>°C</sup></h3>
-                <figure>
-                    <img src="${icon}" alt="${weather[0]['main']}" class="city-icon">
-                    <figcaption class="card-text">${weather[0]['description']}</figcaption>
-                </figure>
-                <button class='btn btn-danger delete-card' onclick='deleteCard(this)'>Remove</button>
-            </li>`;
-            // check if requested city is already on the page
-            if (dataArr.length>0){
-                if(checkID(data)){
-                    // if card not on page
-                    const li = document.createElement('li');
-                    li.classList.add('city');
-                    li.innerHTML = markup;
-                    list.appendChild(li);
-                    dataArr.unshift(data);
-                }
-            }
-            else{
-                // if no cards on page
-                const li = document.createElement('li');
-                li.classList.add('city');
-                li.innerHTML = markup;
-                list.appendChild(li);
-                dataArr.unshift(data);
-            }
-        })
-        .catch((err) => {
-            // catch errors 
-            console.log(err);
-            msg.textContent = 'Requested city was not found';
-            msg.style.display = 'block';
-            
-        });
-    //reset alert banner and form value
-    msg.style.display = 'none';
-    msg.classList.remove('alert-warning');
-    msg.classList.add('alert-danger');
-    form.reset();  
+    try {
+        loadWeather(url)
+    } catch (error){
+        console.log(err);
+        msg.textContent = 'Requested city was not found';
+        msg.style.display = 'block';
+    }
+    finally{
+        //reset alert banner and form value
+        msg.style.display = 'none';
+        msg.classList.remove('alert-warning');
+        msg.classList.add('alert-danger');
+        form.reset();
+    } 
+}
+
+// submit gets click event
+submit.addEventListener('click',  el => {
+    // prevent form from submitting
+    el.preventDefault();
+    main();
 });
 
+// mobile users' touch event listener
+submit.addEventListener('touchstart', el => {
+    el.preventDefault();
+    main();
+})
 
+// loads fetches data from api and builds and appends a card to the page
+async function loadWeather(url){
+    const res = await fetch(url);
+    if (res.status >= 400 && res.status < 600) {
+        msg.textContent = 'Requested city was not found';
+        msg.style.display = 'block';
+    }
+    const data = await res.json();
+    // assign data variables
+    const {main, name, sys, weather} = data;
+    const icon = `https://openweathermap.org/img/wn/${weather[0]['icon']}@2x.png`;
+    // markup for weather card
+    const markup = `
+    <li class="city card col-12">
+        <h2 class="city-name card-title">
+            ${name}, ${sys.country}
+        </h2>
+        <h3 class="city-temp display-4">${Math.round(main.temp)}<sup>°C</sup></h3>
+        <figure>
+            <img src="${icon}" alt="${weather[0]['main']}" class="city-icon">
+            <figcaption class="card-text">${weather[0]['description']}</figcaption>
+        </figure>
+        <button class='btn btn-danger delete-card' onclick='deleteCard(this)'>Remove</button>
+    </li>`;
+    // check if requested city is already on the page
+    if (dataArr.length>0){
+        if(checkID(data)){
+            // if card not on page
+            const li = document.createElement('li');
+            li.classList.add('city');
+            li.innerHTML = markup;
+            list.appendChild(li);
+            dataArr.unshift(data);
+        }
+    }
+    else{
+        // if no cards on page
+        const li = document.createElement('li');
+        li.classList.add('city');
+        li.innerHTML = markup;
+        list.appendChild(li);
+        dataArr.unshift(data);
+    }
+}
+
+// assures that city is not already on the page
 function checkID(resdata){
     for(let i=0; i<dataArr.length; i++){
         // if the requested card's data is in the dataArr, then the card is already on the page
